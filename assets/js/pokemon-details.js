80893b8590099ad9bc7847e6d339e9d31e43063c
Leonardo Silva <leonardosilvaif@hotmail.com>
2023-09-19 18:43:19 -0300

    feat: Styling details page

--- assets/js/pokemon-details.js
+++ assets/js/pokemon-details.js
@@ -1,11 +1,16 @@
 const sectionHeader = document.getElementById('section-header')
-const detailHeader = document.getElementById('detail-header')
+const detailName = document.getElementById('detail-name')
 const queryString = new URLSearchParams(window.location.search)
 const idParam = queryString.get('id')
+
+document.getElementById("go-back").addEventListener("click", () => {
+  history.back();
+});
 
 function loadPokeDetails(pokeId) {
   pokeDetailsApi.getPokemonDetailsById(pokeId).then((pokemonDetails) => {
     sectionHeader.classList.add(pokemonDetails.pokemon.type)
+    detailName.innerHTML += `<span class="name">${pokemonDetails.pokemon.name}</span>`
   })
 }
 
